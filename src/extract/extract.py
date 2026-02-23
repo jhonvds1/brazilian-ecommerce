4b41e67597ca62dc3f6d0ffe1e2afa453a29e421
jonatha silva <jonatha.sviegas@hotmail.com>
2026-02-23 19:29:22 -0300

    extract feito

---
+++ src/extract/extract.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+
+def extract_csvs(folder_path: str) -> dict[str, pd.DataFrame]:
+    dfs = {}
+    for file in os.listdir(folder_path):
+        if file.endswith(".csv"):
+            path = os.path.join(folder_path, file)
+            df = pd.read_csv(path)
+            nome = file.replace(".csv", "")
+            dfs[nome] = df
+    return dfs
+
+
